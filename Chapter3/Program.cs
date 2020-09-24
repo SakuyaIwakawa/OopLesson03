ea33bcdba1e110dcb2a731a98b60c6ffba52d8c0
SakuyaIwakawa <31212@ojs.ac.jp>
2020-09-24 10:29:15 +0900

    5の個数

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -10,6 +10,20 @@ namespace Chapter3
     {
         static void Main(string[] args)
         {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            int count = Count(numbers,5);
+            Console.WriteLine($"5の個数は{count}です");
+        }
+
+        public static int Count(int[] numbers,int num)
+        {
+            int count = 0;
+            foreach(var n in numbers)
+            {
+                if (n == num)
+                    count++;
+            }
+            return count;
         }
     }
 }
